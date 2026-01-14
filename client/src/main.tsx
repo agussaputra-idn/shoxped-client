c5b1875373285f3ab0686644e272a781efba84e5
MacBook Retina <macbookretina@192.168.1.13>
2026-01-15 00:08:07 +0700

    Fix blank screen: Add providers to main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,14 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-// PERBAIKAN: Gunakan BrowserRouter (Standar Website Modern)
-import { BrowserRouter } from 'react-router-dom' 
+import { BrowserRouter } from 'react-router-dom'
+
+// 1. IMPORT PROVIDER (Wajib pakai src/ agar tidak error path)
+import { LanguageProvider } from 'src/context/LanguageContext'
+import { AuthProvider } from 'src/context/authContext'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    {/* Gunakan BrowserRouter agar URL bersih tanpa tanda pagar # */}
     <BrowserRouter>
-      <App />
+      {/* 2. BUNGKUS APP DENGAN PROVIDER */}
+      {/* Urutan: AuthProvider -> LanguageProvider -> App */}
+      <AuthProvider>
+        <LanguageProvider>
+          <App />
+        </LanguageProvider>
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
